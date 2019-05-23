89b975b42ec40e94d4b8b21a43b1e82ce4795d9f
Roniece Ricardo <RonieceRicardo@gmail.com>
2019-05-23 17:33:10 -0400

    initial commit -- crushed it

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,79 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    allSushi: [],
+    sushiIndex: 0,
+    budget: 125
+  }
+
+  componentDidMount(){
+    fetch(API)
+     .then(resp => resp.json())
+     .then(this.fetchSushi)
+  }
+
+  fetchSushi = (data) => {
+    this.setState({
+      allSushi: data
+    })
+    this.addEatenBoolean()
+  }
+
+  addEatenBoolean = () => {
+    this.setState((state) => {
+      state.allSushi.map(sushi => sushi.eaten = false)
+    })
+  }
+
+  sushiSlicer = () => {
+    // determine which sushi get passed into sushiContainer
+    const sushiIndex = this.state.sushiIndex
+    return this.state.allSushi.slice(sushiIndex, sushiIndex + 4)
+  }
+
+  moreSushi = () => {
+    //update state to increase sushiIndex
+    this.setState({
+      sushiIndex: this.state.sushiIndex + 4
+    })
+  }
+
+  eatSushi = (eatenSushi) => {
+    // find the current sushi in allSushi
+    // find & update eaten property
+    // change false to true
+    // build a new array for allSushi including my updated sushi
+    let updatedSushi = this.state.allSushi.map(stateSushi => {
+      if (stateSushi.id === eatenSushi.id && stateSushi.price < this.state.budget){
+        stateSushi.eaten = true
+        this.updateBudget(stateSushi)
+        return stateSushi
+      } else {
+        return stateSushi
+      }
+    })
+    this.setState({
+      allSushi: updatedSushi
+    })
+  }
+
+  updateBudget = (stateSushi) => {
+    let currentBudget = this.state.budget
+      currentBudget -= stateSushi.price
+      this.setState({
+        budget: currentBudget
+      })
+  }
 
   render() {
+    let filteredSushi = this.state.allSushi.filter(sushi => {
+      return sushi.eaten === true
+    })
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer currentSushi={this.sushiSlicer()} moreSushi={this.moreSushi} eatSushi={this.eatSushi}/>
+        <Table filteredSushi={filteredSushi} budget={this.state.budget} updateBudget={this.updateBudget}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+  const { currentSushi, moreSushi, eatSushi } = props
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+         currentSushi.map(aSushi => <Sushi key={aSushi.id} sushi={aSushi} eatSushi={eatSushi}/>)
         }
-        <MoreButton />
+        <MoreButton moreSushi={moreSushi}/>
       </div>
     </Fragment>
   )
